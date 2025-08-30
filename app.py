e23a1f51ff890e8f165bd3a4c0abaac1e15bd245
Kuldeep Sharma <kuldeeps0422@gmail.com>
2025-08-30 23:36:17 +0530

    Fix duplicate route home

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Jobingo Live!"
+    return "Jobingo Live"
+
+@app.route("/about")
+def about():
+    return "About Jobingo"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
